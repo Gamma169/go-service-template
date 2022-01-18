08593fd3e9671f83a8c751ccb778adc3114dfe2d
Rienzi Gokea <RGokea@gmail.com>
2022-01-17 21:49:40 -0500

    Added general endpoint test

---
+++ tests/integration/general-endpoint-test.js
@@ -0,0 +1,51 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+// Make sure you require the health check endpoint test so that it runs first
+require('./health-check-test');
+
+
+const {
+  SERVICE_URL,
+  USER_ID_HEADER,
+} = require('./testcases.js');
+
+const endpointsToTest = [
+  {
+    endpoint: '/user/foobar-models',
+    methods: ['get', 'post'],
+  },
+  {
+    endpoint: '/user/foobar-models/id',
+    methods: ['patch', 'delete'],
+  },
+];
+const improperIdHeaders = [
+  'qwe',
+  '',
+  null,
+];
+
+
+describe("General Endpoint Tests", function() {
+
+  endpointsToTest.forEach(function({endpoint, methods}) {
+    methods.forEach(function(method) {
+      improperIdHeaders.forEach(function(header) {
+
+        it(`should return error on endpoint '${method}' '${endpoint}' if header '${USER_ID_HEADER}' is improper: ${header}`, function(done) {
+          chai.request(SERVICE_URL)[method](endpoint)
+            // .set(USER_ID_HEADER, header)
+            .then(done, function(err) {
+              chai.assert.equal(400, err.status);
+              done();
+            })
+            .catch(done);
+        });
+
+      });
+    });
+  });
+
+});
